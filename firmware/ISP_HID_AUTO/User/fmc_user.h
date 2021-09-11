ff02e4529dcba2fd0060a24559821e0c5a235210
Danny Chou <danchouzhou@gmail.com>
2021-09-11 23:51:40 +0800

    Add ISP_HID_AUTO

---
+++ firmware/ISP_HID_AUTO/User/fmc_user.h
@@ -0,0 +1,43 @@
+/**************************************************************************//**
+ * @file     fmc_user.h
+ * @brief    M480 series FMC driver header file
+ * @version  2.0.0
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#ifndef FMC_USER_H
+#define FMC_USER_H
+
+#include "targetdev.h"
+
+// #define FMC_CONFIG0_ADDR        (FMC_CONFIG_BASE)       /*!< CONFIG 0 Address */
+// #define FMC_CONFIG1_ADDR        (FMC_CONFIG_BASE + 4)   /*!< CONFIG 1 Address */
+
+#define Config0         FMC_CONFIG_BASE
+#define Config1         (FMC_CONFIG_BASE+4)
+
+#define ISPGO           0x01
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Define parameter                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  FMC Macro Definitions                                                                                  */
+/*---------------------------------------------------------------------------------------------------------*/
+#define _FMC_ENABLE_CFG_UPDATE()   (FMC->ISPCTL |=  FMC_ISPCTL_CFGUEN_Msk) /*!< Enable CONFIG Update Function  */
+#define _FMC_DISABLE_CFG_UPDATE()  (FMC->ISPCTL &= ~FMC_ISPCTL_CFGUEN_Msk) /*!< Disable CONFIG Update Function */
+
+
+int FMC_Write_User(uint32_t u32Addr, uint32_t u32Data);
+int FMC_Read_User(uint32_t u32Addr, uint32_t *data);
+int FMC_Erase_User(uint32_t u32Addr);
+void ReadData(uint32_t addr_start, uint32_t addr_end, uint32_t *data);
+void WriteData(uint32_t addr_start, uint32_t addr_end, uint32_t *data);
+int EraseAP(uint32_t addr_start, uint32_t size);
+void UpdateConfig(uint32_t *data, uint32_t *res);
+
+void GetDataFlashInfo(uint32_t *addr, uint32_t *size);
+
+#endif
+
